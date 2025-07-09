9827448bcb3fd6efb793298e7e7b9ff7b255b46f
Yaseer Tasleem <yaseertasleem007@gmail.com>
2025-07-09 23:46:08 +0530

    Add ChevronIcon component and IconProps interface for improved icon usage

---
+++ app/Icons/index.tsx
@@ -0,0 +1,13 @@
+import { IconProps } from '../types';
+
+export const ChevronIcon = ({ className }: IconProps) => (
+	<svg
+		fill='none'
+		stroke='currentColor'
+		viewBox='0 0 24 24'
+		xmlns='http://www.w3.org/2000/svg'
+		className={className}
+	>
+		<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M5 13l4 4L19 7' />
+	</svg>
+);
