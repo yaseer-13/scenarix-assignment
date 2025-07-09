0a6a33896215d5f380292a6da21b9951a8387ac8
Yaseer Tasleem <yaseertasleem007@gmail.com>
2025-07-10 02:18:14 +0530

    Add font data for AI text generation in FontShowcase component

--- app/data/index.ts
+++ app/data/index.ts
@@ -125,3 +125,129 @@ export const tutorials = [
 		link: '/tutorials/image-editing',
 	},
 ];
+
+export const fonts = [
+	{
+		name: 'Dancing Script',
+		text: 'AI Art Generator',
+		className: "font-['Dancing_Script']",
+	},
+	{ name: 'Anton', text: 'AI Art Generator', className: "font-['Anton']" },
+	{
+		name: 'Bebas Neue',
+		text: 'AI Art Generator',
+		className: "font-['Bebas_Neue']",
+	},
+	{ name: 'Charm', text: 'AI Art Generator', className: "font-['Charm']" },
+	{ name: 'Cinzel', text: 'AI Art Generator', className: "font-['Cinzel']" },
+	{
+		name: 'Comfortaa',
+		text: 'AI Art Generator',
+		className: "font-['Comfortaa']",
+	},
+	{
+		name: 'Courgette',
+		text: 'AI Art Generator',
+		className: "font-['Courgette']",
+	},
+	{
+		name: 'Crimson Text',
+		text: 'AI Art Generator',
+		className: "font-['Crimson_Text']",
+	},
+	{ name: 'Dosis', text: 'AI Art Generator', className: "font-['Dosis']" },
+	{
+		name: 'Great Vibes',
+		text: 'AI Art Generator',
+		className: "font-['Great_Vibes']",
+	},
+	{
+		name: 'Indie Flower',
+		text: 'AI Art Generator',
+		className: "font-['Indie_Flower']",
+	},
+	{
+		name: 'Josefin Sans',
+		text: 'AI Art Generator',
+		className: "font-['Josefin_Sans']",
+	},
+	{ name: 'Kalam', text: 'AI Art Generator', className: "font-['Kalam']" },
+	{ name: 'Lato', text: 'AI Art Generator', className: "font-['Lato']" },
+	{
+		name: 'Libre Baskerville',
+		text: 'AI Art Generator',
+		className: "font-['Libre_Baskerville']",
+	},
+	{ name: 'Lobster', text: 'AI Art Generator', className: "font-['Lobster']" },
+	{
+		name: 'Montserrat',
+		text: 'AI Art Generator',
+		className: "font-['Montserrat']",
+	},
+	{
+		name: 'Open Sans',
+		text: 'AI Art Generator',
+		className: "font-['Open_Sans']",
+	},
+	{ name: 'Oswald', text: 'AI Art Generator', className: "font-['Oswald']" },
+	{
+		name: 'Pacifico',
+		text: 'AI Art Generator',
+		className: "font-['Pacifico']",
+	},
+	{
+		name: 'Playfair Display',
+		text: 'AI Art Generator',
+		className: "font-['Playfair_Display']",
+	},
+	{ name: 'Poppins', text: 'AI Art Generator', className: "font-['Poppins']" },
+	{
+		name: 'Quicksand',
+		text: 'AI Art Generator',
+		className: "font-['Quicksand']",
+	},
+	{ name: 'Raleway', text: 'AI Art Generator', className: "font-['Raleway']" },
+	{ name: 'Roboto', text: 'AI Art Generator', className: "font-['Roboto']" },
+	{
+		name: 'Roboto Condensed',
+		text: 'AI Art Generator',
+		className: "font-['Roboto_Condensed']",
+	},
+	{
+		name: 'Roboto Mono',
+		text: 'AI Art Generator',
+		className: "font-['Roboto_Mono']",
+	},
+	{
+		name: 'Roboto Slab',
+		text: 'AI Art Generator',
+		className: "font-['Roboto_Slab']",
+	},
+	{ name: 'Rubik', text: 'AI Art Generator', className: "font-['Rubik']" },
+	{
+		name: 'Source Sans Pro',
+		text: 'AI Art Generator',
+		className: "font-['Source_Sans_Pro']",
+	},
+	{
+		name: 'Source Serif Pro',
+		text: 'AI Art Generator',
+		className: "font-['Source_Serif_Pro']",
+	},
+	{
+		name: 'Space Grotesk',
+		text: 'AI Art Generator',
+		className: "font-['Space_Grotesk']",
+	},
+	{
+		name: 'Space Mono',
+		text: 'AI Art Generator',
+		className: "font-['Space_Mono']",
+	},
+	{ name: 'Ubuntu', text: 'AI Art Generator', className: "font-['Ubuntu']" },
+	{
+		name: 'Work Sans',
+		text: 'AI Art Generator',
+		className: "font-['Work_Sans']",
+	},
+];
